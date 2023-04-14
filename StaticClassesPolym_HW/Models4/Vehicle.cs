997f00a50d25ab964c0c88269f99fc349f408438
annabananna <ana_bogdanovska@yahoo.com>
2023-04-14 23:43:08 +0200

    Add files via upload

---
+++ StaticClassesPolym_HW/Models4/Vehicle.cs
@@ -0,0 +1,15 @@
+﻿namespace Models4
+{
+    public class Vehicle
+    {
+        public int Id { get; set; }
+        public string Type { get; set; }
+        public int YearOfProduction { get; set; }
+        public string BatchNumber { get; set; }
+
+        public virtual string PrintVehicle()
+        {
+            return $"{Id}. {Type} [{YearOfProduction}]";
+        }
+    }
+}
